e7f5760a72b577dffb885d3fd240bea892e1f6e1
fahizrahmank <fahizrahmank@gmail.com>
2023-05-16 16:01:45 +0530

    update

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -9,31 +9,31 @@ const Footer = () => {
   <ul  >
   <h1>Shop</h1>
 
-    <Link to="/men" ><li><a href="#">Shop Men</a></li></Link>
-    <Link to="/women" ><li><a href="#">Shop Women</a></li></Link>
-    <li><a href="#">Lookbook</a></li>
-    <li><a href="#">Gift Card</a></li>
-    <li><a href="#">Sale</a></li>
+    <Link to="/men" ><li>Shop Men</li></Link>
+    <Link to="/women" ><li>Shop Women</li></Link>
+    <li>Lookbook</li>
+    <li>Gift Card</li>
+    <li>Sale</li>
   </ul>
 
   <ul>
   <h1>About</h1>
 
-    <li><a href="#">Our Story</a></li>
-    <li><a href="#">Our Materials</a></li>
-    <li><a href="#">Our Value</a></li>
-    <li><a href="#">Sustainability</a></li>
-    <li><a href="#">Manufacture</a></li>
+    <li>Our Story</li>
+    <li>Our Materials</li>
+    <li>Our Value</li>
+    <li>Sustainability</li>
+    <li>Manufacture</li>
   </ul>
 
   <ul>
   <h1>Need Help?</h1>
 
-    <li><a href="#">FAQs</a></li>
-    <li><a href="#">Shipping & Returns</a></li>
-    <li><a href="#">Shoe Care</a></li>
-    <li><a href="#">Size Chart</a></li>
-    <li><a href="#">Contact Us</a></li>
+    <li>FAQs</li>
+    <li>Shipping & Returns</li>
+    <li>Shoe Care</li>
+    <li>Size Chart</li>
+    <li>Contact Us</li>
   </ul>
 </footer>
 
